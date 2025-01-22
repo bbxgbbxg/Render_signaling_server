6ca8397d8f25cd2bc3928846539ce746c6e1873e
bbxgbbxg <165761860+bbxgbbxg@users.noreply.github.com>
2025-01-22 10:13:37 -0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const { ExpressPeerServer } = require('peer');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+const server = app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server is running on port ${process.env.PORT || 3000}`);
+});
+
+const peerServer = ExpressPeerServer(server, {
+    path: '/myapp',
+    debug: true
+});
+
+app.use('/', peerServer);
+
+// Basic health check endpoint
+app.get('/health', (req, res) => {
+    res.send('Server is running');
+});
